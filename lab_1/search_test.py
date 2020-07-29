a24ece3ebdaad42ec2b2471b6c16ba4230100182
Sailesh Dahal <saileshbro@gmail.com>
2020-07-29 13:23:40 +0545

    mofified test_cases

--- lab_1/search_test.py
+++ lab_1/search_test.py
@@ -12,11 +12,11 @@ class TestSearch(unittest.TestCase):
     def test_binary_search(self):
         data = [1, 2, 3, 4, 5, 6, 7, 8, 9]
         self.assertEqual(is_sorted(data), True)
-        self.assertEqual(binary_search(data, 1,), 0)
-        self.assertEqual(binary_search(data, 14), -1)
-        self.assertEqual(binary_search(data, -1), -1)
-        self.assertEqual(binary_search(data, 2), 1)
-        self.assertEqual(binary_search(data, 4), 3)
+        self.assertEqual(binary_search(data, 1, 0, len(data)-1), 0)
+        self.assertEqual(binary_search(data, 14, 0, len(data)-1), -1)
+        self.assertEqual(binary_search(data, -1, 0, len(data)-1), -1)
+        self.assertEqual(binary_search(data, 2, 0, len(data)-1), 1)
+        self.assertEqual(binary_search(data, 4, 0, len(data)-1), 3)
 
 
 if __name__ == "__main__":
